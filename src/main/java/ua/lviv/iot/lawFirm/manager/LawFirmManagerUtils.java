aee9397d2a6ff586b9b9c90b91e01c0f7d79dba3
Svyatoslav <svyatoslavtrostynski24@gmail.com>
2020-03-02 04:27:31 +0200

    Added the manager(Here is class for search and another one for sort)

---
+++ src/main/java/ua/lviv/iot/lawFirm/manager/LawFirmManagerUtils.java
@@ -0,0 +1,18 @@
+package ua.lviv.iot.lawFirm.manager;
+
+import java.util.List;
+
+import ua.lviv.iot.lawFirm.model.Lawyer;
+import ua.lviv.iot.lawFirm.model.SortType;
+
+public class LawFirmManagerUtils {
+
+	public static void sortByName(SortType sortType, List<Lawyer> lawyers) {
+
+	}
+
+	public static void sortByServices(SortType sortType, List<Lawyer> lawyers) {
+
+	}
+
+}
