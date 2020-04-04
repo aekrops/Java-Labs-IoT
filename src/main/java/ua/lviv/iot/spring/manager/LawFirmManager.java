47044a034c7e00a62ae97928e40b4c289c6dc9d2
aekrops <svyatoslavtrostynski24@gmail.com>
2020-04-04 20:44:42 +0300

    changed directory for base folders

---
+++ src/main/java/ua/lviv/iot/spring/manager/LawFirmManager.java
@@ -0,0 +1,48 @@
+package ua.lviv.iot.lawFirm.spring.manager;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import ua.lviv.iot.lawFirm.spring.model.Lawyer;
+import ua.lviv.iot.lawFirm.spring.model.Services;
+
+
+public class LawFirmManager {
+    protected List<Lawyer> lawyers = new LinkedList<>();
+
+    public LawFirmManager(List<Lawyer> lawyers) {
+        this.lawyers = lawyers;
+    }
+
+    public LawFirmManager() {
+    }
+
+    public List<Lawyer> findLawyersByServices(Services serviceToSearch, List<Lawyer> lawyers) {
+
+        List<Lawyer> foundLawyers = new LinkedList<Lawyer>();
+
+        for (Lawyer lawyerToCheck : lawyers) {
+            List<Services> services = lawyerToCheck.getServices();
+
+            int count = 0;
+
+            for (Services service : services) {
+                if (serviceToSearch == service)
+                    count++;
+            }
+            if (count > 0) {
+                foundLawyers.add(lawyerToCheck);
+            }
+        }
+
+        return foundLawyers;
+    }
+
+    public List<Lawyer> getLawyers() {
+        return lawyers;
+    }
+
+    public void addLawyers(Lawyer lawyer) {
+        lawyers.add(lawyer);
+    }
+}
